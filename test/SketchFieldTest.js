d5ffaee6b72f85c9f11cf5f54b8c145b302921a3
pomelyu <pomelyu5199@icloud.com>
2017-11-13 10:34:54 +0800

    implement unit test for 'Drag to create rectangle'

--- test/SketchFieldTest.js
+++ test/SketchFieldTest.js
@@ -7,6 +7,26 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import SketchField from '../src/SketchField';
 
+function objectFromDrag(canvas, from = { x: 0, y: 0 }, to = { x: 10, y: 10 }, id) {
+    function MouseEventPositionGenerator(pos = { x: 0, y: 0 }) {
+        const eventX = ['x', 'pageX', 'screenX', 'clientX', 'offsetX'];
+        const eventY = ['y', 'pageY', 'screenY', 'clientY', 'offsetY'];
+        const generated = {};
+        eventX.forEach(key => generated[key] = pos.x);
+        eventY.forEach(key => generated[key] = pos.y);
+        return generated;
+    }
+
+    canvas.trigger('mouse:down', { e: MouseEventPositionGenerator(from) });
+    canvas.trigger('mouse:move', { e: MouseEventPositionGenerator(to) });
+    canvas.trigger('mouse:up', { e: MouseEventPositionGenerator(to) });
+    // Get the last object as the last created object
+    const objects = canvas.getObjects();
+    const newObj = objects[objects.length - 1];
+    id && (newObj.id = id);
+    return newObj;
+}
+
 describe('SketchField', () => {
 
     it('Loads Normally', () => {
@@ -18,4 +38,38 @@ describe('SketchField', () => {
         expect(TestUtils.findRenderedDOMComponentWithTag(sketch, 'canvas')).to.exist;
     });
 
+    it('Drag to create rectangle', () => {
+        const sketch = TestUtils.renderIntoDocument(<SketchField tool={'rectangle'} />);
+        const canvas = sketch._fc;
+        expect(canvas).to.exist;
+        
+        const startPt = { x: 10, y: 10 };
+        const endPt = { x: 40, y: 50 };
+        const bounding = {
+            left: startPt.x,
+            top: startPt.y,
+            width: endPt.x - startPt.x,
+            height: endPt.y - startPt.y
+        }
+
+        // From left-top to right-bottom
+        objectFromDrag(canvas, startPt, endPt);
+
+        // Check the rectangle existed
+        expect(canvas.getObjects()[0]).to.exist;
+        const rect1 = canvas.getObjects()[0];
+        expect(rect1.type).equal('rect');
+
+        // Check the rectangle dimension
+        expect({ left: rect1.left, top: rect1.top, width: rect1.width, height: rect1.height }).eql(bounding);
+
+        canvas.remove(rect1);
+        // From right-bottom to left-top;
+        objectFromDrag(canvas, endPt, startPt);
+        const rect2 = canvas.getObjects()[0];
+        expect(rect2.type).equal('rect');
+
+        // Check the rectangle dimension
+        expect({ left: rect2.left, top: rect2.top, width: rect2.width, height: rect2.height }).eql(bounding);
+    });
 });
