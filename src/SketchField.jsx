bb4bc72a3558d69ee8382ac58154f6a22d1665f3
Youssef KH <me@youssefkh.com>
2020-05-24 09:12:48 +0000

    fix fillColor not updating

--- src/SketchField.jsx
+++ src/SketchField.jsx
@@ -701,7 +701,7 @@ class SketchField extends PureComponent {
       }
     }
 
-    if (this.props.lineColor !== prevProps.lineColor) {
+    if (this.props.lineColor !== prevProps.lineColor || this.props.fillColor !== prevProps.fillColor) {
       if(this._selectedTool){
         this._selectedTool.configureCanvas(this.props);
       }
